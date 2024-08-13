5b45fc217ace25c5f15e5902cdf0e3faab1f70cf
vdesimone <173499012+vdesimone@users.noreply.github.com>
2024-08-13 19:35:57 -0400

    added scroll to top feature

--- script.js
+++ script.js
@@ -30,3 +30,30 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 })
+
+//Scroll to Top
+document.querySelector("#to-top").addEventListener("click", () => {
+  let topInterval = setInterval(() => {
+    let arrowTop = document.body.scrollTop > 0 ? document.body : document.documentElement;
+
+    if(arrowTop.scrollTop > 0) {
+      arrowTop.scrollTop = arrowTop.scrollTop - 50;
+    }
+    if(arrowTop.scrollTop < 1) {
+      clearInterval(topInterval);
+    }
+  }, 10)
+}, false);
+
+function showScroll() {
+  let topButton = document.getElementById("to-top");
+  if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    topButton.classList.add("show")
+  } else {
+    topButton.classList.remove("show")
+  }
+}
+
+window.onscroll = () => {
+  showScroll();
+}
