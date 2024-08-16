00155843e3f384b04df937e5f8a1b7af958fd3f4
vdesimone <173499012+vdesimone@users.noreply.github.com>
2024-08-16 17:15:57 -0400

    added functionality and styling to accessories section

--- script.js
+++ script.js
@@ -155,3 +155,103 @@ function switchVideo(videoElement) {
 
 const videoElement = document.getElementById("video");
 switchVideo(videoElement);
+
+// Cycle through accessories
+const travelCases = {
+  case1: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/My%20Nintendo%20Store/EN-US/Nintendo%20Switch%20Accessories/Cases/nintendo-switch-travel-case-glow-super-star-120767/120767-pdp-nintendo-switch-travel-case-super-mario-star-front-1200x675",
+    description: "Nintendo Switch™ Travel Case Glow - Super Star",
+  },
+  case2: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/en_US/products/accessories/nintendo-switch/system-cases-and-bags/protection-case-animal-crossing-117923/117923-powera-protection-case-animal-crossing-front-1200x675",
+    description: "Protection Case - Animal Crossing™",
+  },
+  case3: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/en_US/products/accessories/nintendo-switch/system-cases-and-bags/protection-case-kirby-117924/117924-powera-protection-case-kirby-front-1200x675",
+    description: "Protection Case - Kirby™",
+  },
+};
+
+const wiredControllers = {
+  controller1: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/My%20Nintendo%20Store/EN-US/Nintendo%20Switch%20Accessories/Controllers/rematch-wired-controller-mario-escape-120768/120768-pdp-nintendo-switch-rematch-wired-controller-mario-escape-front-1200x675",
+    description: "REMATCH Wired Controller: Mario™ Escape",
+  },
+  controller2: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/en_US/products/accessories/nintendo-switch/controllers/pro-controllers-and-gamepads/faceoff-deluxe-audio-wired-controller-animal-crossing-tom-nook-117865/117865-pdp-faceoff-deluxe-plus-audio-wired-controller-animal-crossing-tom-nook-front-1200x675",
+    description: "Faceoff™ Deluxe+ Audio Wired Controller: Animal Crossing™: Tom Nook",
+  },
+  controller3: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/en_US/products/accessories/nintendo-switch/controllers/pro-controllers-and-gamepads/enhanced-wired-controller-kirby-117921/117921-powera-enhanced-wireless-controller-kirby-front-1200x675",
+    description: "Enhanced Wired Controller - Kirby™",
+  },
+};
+
+const joyCons = {
+  joyCon1: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/en_US/products/accessories/nintendo-switch/controllers/joy-con-controllers/joy-con-neon-red-neon-blue/104882-nintendo-switch-joy-con-red-blue-package-1200x675",
+    description: "Joy-Con™ (L)/(R) Neon Red/Neon Blue",
+  },
+  joyCon2: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/My%20Nintendo%20Store/EN-US/Nintendo%20Switch%20Accessories/Controllers/joy-con-l-r-pastel-purple-pastel-green-117078/117078-nintendo-switch-joy-con-set-pastel-purple-l-pastel-green-r-box-1200x675",
+    description: "Joy-Con™ (L)/(R) Pastel Purple / Pastel Green",
+  },
+  joyCon3: {
+    src: "https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_801/b_white/f_auto/q_auto/ncom/My%20Nintendo%20Store/EN-US/Nintendo%20Switch%20Accessories/Controllers/joy-con-l-r-pastel-pink-120776/120776-nintendo-joy-con-set-pastel-pink-package-1200x675",
+    description: "Joy-Con™ (L)/(R) Pastel Pink",
+  },
+};
+
+const travelCaseImage = document.getElementById("travel-case-img");
+const controllerImage = document.getElementById("controller-img");
+const joyConImage = document.getElementById("joy-con-img");
+
+const travelCaseDescription = document.getElementById("travel-case-desc");
+const controllerDescription = document.getElementById("controller-desc");
+const joyConDescription = document.getElementById("joy-con-desc");
+
+function changeAccessory() {
+  const travelCaseKeys = Object.keys(travelCases);
+  const controllerKeys = Object.keys(wiredControllers);
+  const joyConKeys = Object.keys(joyCons);
+  let index = 0;
+
+  setInterval(() => {
+    const currentTravelCase = travelCases[travelCaseKeys[index]];
+    travelCaseImage.src = currentTravelCase.src;
+    travelCaseDescription.textContent = currentTravelCase.description;
+
+    const currentController = wiredControllers[controllerKeys[index]];
+    controllerImage.src = currentController.src;
+    controllerDescription.textContent = currentController.description;
+
+    const currentJoyCon = joyCons[joyConKeys[index]];
+    joyConImage.src = currentJoyCon.src;
+    joyConDescription.textContent = currentJoyCon.description;
+
+    // Fade in effect
+    travelCaseImage.style.opacity = 1;
+    controllerImage.style.opacity = 1;
+    joyConImage.style.opacity = 1;
+
+    setTimeout(() => {
+      travelCaseImage.style.transition = "opacity 1s";
+      travelCaseImage.style.opacity = 1;
+      controllerImage.style.transition = "opacity 1s";
+      controllerImage.style.opacity = 1;
+      joyConImage.style.transition = "opacity 1s";
+      joyConImage.style.opacity = 1
+    }, 1000);
+
+    // Fade out effect
+    setTimeout(() => {
+      travelCaseImage.style.opacity = 0;
+      controllerImage.style.opacity = 0;
+      joyConImage.style.opacity = 0;
+    }, 7000);
+
+    index = (index + 1) % travelCaseKeys.length;
+  }, 8000);
+}
+
+changeAccessory();
